acccc85f2684cbd71323d84a46736eb6fef11b9a
mayurchandpara <mayurchandpara_gmail.com>
2018-04-24 02:47:52 +0530

    Frontend pushing like the whole list and the searchbox is just there

---
+++ src/robots.js
@@ -0,0 +1,62 @@
+export const robots = [
+{
+	id : 1,
+	name:'Chrysantha Jolliff',
+	username: 'hgfdsg',
+	email:'ch.jollif@egl-inc.info'
+},
+{
+	id : 2,
+	name:'Frasier Claypool',
+	username: 'sadg',
+	email:'fras.claypoo@egl-inc.info'
+},
+{
+	id : 3,
+	name:'Arwin Rolen',
+	username: 'ljhs',
+	email:'arwinrolen@diaperstack.com'
+},
+{
+	id : 4,
+	name:'Starr Wathen',
+	username: 'jksfd',
+	email:'starr.wath@autozone-inc.info'
+},
+{
+	id : 5,
+	name:'Daid Warner',
+	username: 'ksdahg',
+	email:'dav_warner@shdf.com'
+},
+{
+	id : 6,
+	name:'Peter Mentoman',
+	username: 'sdaf',
+	email:'mento_pete@sdg.com'
+},
+{
+	id : 7,
+	name:'Mike John',
+	username: 'jhdas',
+	email:'Mike_joh@gjh.com'
+},
+{
+	id : 8,
+	name:'Mikel Fred',
+	username: 'sdghg',
+	email:'Mk_els@ht.com'
+},
+{
+	id : 9,
+	name:'Donald Trusk',
+	username: 'ahasgdsg',
+	email:'trusk_donald@gam.com'
+},
+{
+	id : 10,
+	name:'Mark Zuker',
+	username: 'dasg',
+	email:'zuku_marky@sdg.com'
+},
+]
